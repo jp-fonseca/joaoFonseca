8974e33980a669faaa6d38be026a469acdca5cff
fonsecajp <jopalves39@gmail.com>
2022-07-22 12:18:46 -0300

    Modeling and Presentation added to project.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,22 @@
-DELETE FROM `author`
+DELETE FROM `author`;
 INSERT INTO `author` (`id`,`email`,`name`,`password`) VALUES
 (1,'joao@gmail.com','Joao Paulo','1231149'),
 (2,'ana@gmail.com','Ana Paula','12355'),
 (3,'maria@gmail.com','Maria Clara','123578');
 
-DELETE FROM `media`
+DELETE FROM `media`;
 INSERT INTO `media`(`release_year`,`genre`,`title`,`score`) VALUES
 ('2010','HORROR','THE EXORCISM OF EMILY ROSE',7.9),
 ('2008','COMEDY','THE WHITE CHICKS',8.9),
 ('2020','ACTION','CAPTAIN MARVEL',5.9);
 
-DELETE FROM `topic`
+DELETE FROM `topic`;
 INSERT INTO `topic`(`description`,`status`,`title`,`author_id`,`media_id`) VALUES
 ('BEST COMEDY EVER! BLA BLA BLA BLA BLA','NOT_REPLIED','THIS IS A MUST WATCH','1','2'),
 ('THATS THE POWER OF THIS MOVIE.','REPLIED','IM AWAKE 5 DAYS STRAIGHT','2','1'),
 ('THIS ONE KINDA MEH.','CLOSED','MEH','3','3');
 
-DELETE FROM `reply`
+DELETE FROM `reply`;
 INSERT INTO `reply` (`description`,`author_id`,`topic_id`) VALUES
 ('LOL, YOURE DAMN RIGHT','3','2'),
 ('IM NOT WATCHING THIS','2','3'),
